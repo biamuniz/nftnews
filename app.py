5d06e96a93bae3bffa1441ef4e62e4f4f3e988fc
Bianca <41485608+biamuniz@users.noreply.github.com>
2021-12-20 21:38:43 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import csv
+import io
+import requests
+from flask import Flask
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    arquivo = open("templates/home.html")
+    return arquivo.read()
+
+@app.route("/sobre")
+def sobre():
+    arquivo = open("templates/sobre.html")
+    return arquivo.read()
+
+@app.route("/nftnews")
+def tweetify():
+    arquivo = open("templates/nftnews.html")
+    return arquivo.read()
+
+
+def nftnews():
+    from GoogleNews import GoogleNews
+    import pandas as pd
+    googlenews=GoogleNews(period='d')
+    googlenews.setlang('pt')
+    googlenews.search('NFT')
+    result=googlenews.result()
+    df=pd.DataFrame(result)
+    del df['datetime']
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    return df
