b9bfcb999dada99da9264970c9f0d9fe1850b1b7
Bianca <41485608+biamuniz@users.noreply.github.com>
2021-12-20 23:02:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-import csv
-import io
+import base64
+import json
+import os
+
+import pandas as pd
 import requests
-from flask import Flask
-import pandas as pd
+
+from flask import Flask, render_template
 from GoogleNews import GoogleNews
-import pandas as pd
 
 app = Flask(__name__)
+
+spreadsheet_id = os.environ["GOOGLE_SHEET_ID"]
+conteudo_codificado = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+conteudo = base64.b64decode(conteudo_codificado)
+credentials = json.loads(conteudo)
+service_account = gspread.service_account_from_dict(credentials)
+spreadsheet = service_account.open_by_key(spreadsheet_id) 
+worksheet = spreadsheet.worksheet("Página1")
 
 @app.route("/")
 def hello_world():
@@ -19,10 +29,9 @@ def sobre():
     return arquivo.read()
 
 @app.route("/nftnews")
-def tweetify():
+def NFTnews():
     arquivo = open("templates/nftnews.html")
     return arquivo.read()
-
 
 def nftnews():
     googlenews=GoogleNews(period='d')
