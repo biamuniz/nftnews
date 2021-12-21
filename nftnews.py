0f4730e42ca8be864837d54f549bbb51fbcc21c6
Bianca <41485608+biamuniz@users.noreply.github.com>
2021-12-20 23:05:50 -0300

    Update nftnews.py

--- nftnews.py
+++ nftnews.py
@@ -1,3 +1,20 @@
+import base64
+import json
+import os
+
+import pandas as pd
+import requests
+
+from GoogleNews import GoogleNews
+
+spreadsheet_id = os.environ["GOOGLE_SHEET_ID"]
+conteudo_codificado = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+conteudo = base64.b64decode(conteudo_codificado)
+credentials = json.loads(conteudo)
+service_account = gspread.service_account_from_dict(credentials)
+spreadsheet = service_account.open_by_key(spreadsheet_id) 
+worksheet = spreadsheet.worksheet("Página1")
+
 def nftnews():
     from GoogleNews import GoogleNews
     import pandas as pd
@@ -8,4 +25,4 @@ def nftnews():
     df=pd.DataFrame(result)
     del df['datetime']
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
-    return df
+    return worksheet
