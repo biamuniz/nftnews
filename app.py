7efdc79816feb347bba02d174444fb971fea1543
Bianca <41485608+biamuniz@users.noreply.github.com>
2021-12-21 12:12:43 -0300

    Tira o def nftnews

--- app.py
+++ app.py
@@ -22,15 +22,3 @@ worksheet = spreadsheet.worksheet("Página1")
 def hello_world():
     arquivo = open("templates/home.html")
     return arquivo.read()
-
-@app.route("/nftnews")
-def nftnews():
-    googlenews=GoogleNews(period='d')
-    googlenews.setlang('pt')
-    googlenews.search('NFT')
-    result=googlenews.result()
-    df=pd.DataFrame(result)
-    del df['datetime']
-    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
-    arquivo = open("templates/nftnews.html")
-    return arquivo.read(), worksheet
